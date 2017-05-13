c2a8dae800d251328f4ef4ef382a3dc5b0be6362
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-05-13 20:54:19 +0100

    Durations added if project, activity and weekday match

--- t.go
+++ t.go
@@ -10,6 +10,7 @@ import (
 	"os/user"
 	"strconv"
 	s "strings"
+	"text/tabwriter"
 
 	"fmt"
 	"time"
@@ -38,10 +39,10 @@ func main() {
 	// Get data summarized
 	summary := GetDataSummary(data)
 
-	// TODO get week date to add it to excel sheet
-
 	// Create test xlsx file
 	WriteListToXlxs("Week date goes here", summary, "test.xlsx")
+
+	GetDataToExport(summary)
 }
 
 // DataSummaryRecord : struct to store every record value
@@ -50,6 +51,12 @@ type DataSummaryRecord struct {
 	activity string
 	date     time.Time
 	duration float64
+}
+
+// DataToExport : struct to store week activity durations
+type DataToExport struct {
+	project, activity                                                                                                      string
+	mondayDuration, tuesdayDuration, wednesdayDuration, thursdayDuration, fridayDuration, saturdayDuration, sundayDuration float64
 }
 
 // GetDataSummary : return a table with duplicated records united
@@ -92,7 +99,124 @@ func GetDataSummary(data [][]string) []DataSummaryRecord {
 		}
 
 	}
+
 	return dataSummary
+}
+
+// GetDataToExport : return a table with information sorted ready to export
+func GetDataToExport(data []DataSummaryRecord) []DataToExport {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+	for i := range data {
+		// Combine all data in a single string
+		var s string
+		s = data[i].project + "\t" + data[i].activity + "\t"
+		s = s + data[i].date.Format("2006.01.02") + "\t"
+		s = s + data[i].date.Weekday().String() + "\t"
+		s = s + strconv.FormatFloat(data[i].duration, 'f', -1, 64)
+		fmt.Fprintln(w, s)
+	}
+	w.Flush()
+	fmt.Println("---")
+	var dataExport []DataToExport
+	for i := range data {
+		x := DataToExport{}
+		activityFound := false
+		// Check if this activity has been already added to dataExport
+		for ii := range dataExport {
+			if dataExport[ii].project == data[i].project && dataExport[ii].activity == data[i].activity {
+				// edit existing tasks to add values
+				switch weekday := data[i].date.Weekday().String(); weekday {
+				case "Monday":
+					if dataExport[ii].mondayDuration != 0 {
+						dataExport[ii].mondayDuration = dataExport[ii].mondayDuration + data[i].duration
+					} else {
+						dataExport[ii].mondayDuration = data[i].duration
+					}
+				case "Tuesday":
+					if dataExport[ii].tuesdayDuration != 0 {
+						dataExport[ii].tuesdayDuration = dataExport[ii].tuesdayDuration + data[i].duration
+					} else {
+						dataExport[ii].tuesdayDuration = data[i].duration
+					}
+				case "Wednesday":
+					if dataExport[ii].tuesdayDuration != 0 {
+						dataExport[ii].wednesdayDuration = dataExport[ii].wednesdayDuration + data[i].duration
+					} else {
+						dataExport[ii].wednesdayDuration = data[i].duration
+					}
+				case "Thursday":
+					if dataExport[ii].thursdayDuration != 0 {
+						dataExport[ii].thursdayDuration = dataExport[ii].thursdayDuration + data[i].duration
+					} else {
+						dataExport[ii].thursdayDuration = data[i].duration
+					}
+				case "Friday":
+					if dataExport[ii].fridayDuration != 0 {
+						dataExport[ii].fridayDuration = dataExport[ii].fridayDuration + data[i].duration
+					} else {
+						dataExport[ii].fridayDuration = data[i].duration
+					}
+				case "Saturday":
+					if dataExport[ii].saturdayDuration != 0 {
+						dataExport[ii].saturdayDuration = dataExport[ii].saturdayDuration + data[i].duration
+					} else {
+						dataExport[ii].saturdayDuration = data[i].duration
+					}
+				case "Sunday":
+					if dataExport[ii].sundayDuration != 0 {
+						dataExport[ii].sundayDuration = dataExport[ii].sundayDuration + data[i].duration
+					} else {
+						dataExport[ii].sundayDuration = data[i].duration
+					}
+				}
+
+				activityFound = true
+				break
+			}
+		}
+		if activityFound == false {
+			// add new task
+			x.project = data[i].project
+			x.activity = data[i].activity
+			switch weekday := data[i].date.Weekday().String(); weekday {
+			case "Monday":
+				x.mondayDuration = data[i].duration
+			case "Tuesday":
+				x.tuesdayDuration = data[i].duration
+			case "Wednesday":
+				x.wednesdayDuration = data[i].duration
+			case "Thursday":
+				x.thursdayDuration = data[i].duration
+			case "Friday":
+				x.fridayDuration = data[i].duration
+			case "Saturday":
+				x.saturdayDuration = data[i].duration
+			case "Sunday":
+				x.sundayDuration = data[i].duration
+			}
+			dataExport = append(dataExport, x)
+		}
+	}
+
+	w = tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+	for i := range dataExport {
+		// Combine all data in a single string
+		var s string
+		s = dataExport[i].project + "\t" + dataExport[i].activity + "\t"
+		s = s + strconv.FormatFloat(dataExport[i].mondayDuration, 'f', -1, 64) + "\t"
+		s = s + strconv.FormatFloat(dataExport[i].tuesdayDuration, 'f', -1, 64) + "\t"
+		s = s + strconv.FormatFloat(dataExport[i].wednesdayDuration, 'f', -1, 64) + "\t"
+		s = s + strconv.FormatFloat(dataExport[i].thursdayDuration, 'f', -1, 64) + "\t"
+		s = s + strconv.FormatFloat(dataExport[i].fridayDuration, 'f', -1, 64) + "\t"
+		s = s + strconv.FormatFloat(dataExport[i].saturdayDuration, 'f', -1, 64) + "\t"
+		s = s + strconv.FormatFloat(dataExport[i].sundayDuration, 'f', -1, 64)
+
+		fmt.Fprintln(w, s)
+	}
+	w.Flush()
+
+	var ret []DataToExport
+	return ret
 }
 
 // ParseDuration : return a float64 with the number of hours rounded to the nearest 0.25h
